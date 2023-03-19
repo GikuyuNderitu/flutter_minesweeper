44118a38c1eb264b308f4a5321e726856f5f3138
GikuyuNderitu <GikuyuNderitu@gmail.com>
2023-03-18 20:32:01 -0700

    Add rough styling to the cell view

--- lib/game/game_view.dart
+++ lib/game/game_view.dart
@@ -37,15 +37,69 @@ class CellView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
     return MouseRegion(
       cursor: SystemMouseCursors.click,
       child: GestureDetector(
         onTap: () => context.read<GameCubit>().move(point),
-        child: Padding(
+        child: Container(
+          color: _cellToColor(cell),
           padding: const EdgeInsets.all(58.0),
-          child: Text(cell.value.toString()),
+          child: Text(
+            number,
+            style: textTheme.headlineLarge?.copyWith(color: textColor),
+          ),
         ),
       ),
     );
+  }
+
+  Color get textColor {
+    switch (cell.value) {
+      case initialCellValue:
+      case mineCellValue:
+      case emptyCellValue:
+        return Colors.transparent;
+      case 1:
+        return Colors.blue;
+      case 2:
+        return Colors.green;
+      case 3:
+        return Colors.red;
+      case 4:
+        return Colors.deepPurple;
+      case 5:
+        return Colors.brown;
+      case 6:
+        return Colors.yellow;
+      case 7:
+        return Colors.purple;
+      case 8:
+        return Colors.orange;
+    }
+
+    throw ArgumentError('Unexpected Cell value');
+  }
+
+  String get number {
+    switch (cell.value) {
+      case initialCellValue:
+      case mineCellValue:
+      case emptyCellValue:
+        return '';
+      default:
+        return cell.value.toString();
+    }
+  }
+}
+
+Color _cellToColor(Cell cell) {
+  switch (cell.value) {
+    case initialCellValue:
+    case mineCellValue:
+      return Colors.grey;
+    case emptyCellValue:
+    default:
+      return const Color.fromARGB(255, 212, 190, 182);
   }
 }
